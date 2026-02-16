ca6c6f8840ec6676f1177bf88b275aa779c510d2
Uki <nclniculina@gmail.com>
2026-02-17 00:36:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Находим кнопку по id и вешаем событие click
+document.getElementById('convertBtn').addEventListener('click', async () => {
+   // берём значения из полей
+   const amount = parseFloat(document.getElementById('amount').value);
+   const from = document.getElementById('fromCurrency').value;
+   const to = document.getElementById('toCurrency').value;
+
+   // проверка на корректное число 
+   if (isNaN(amount)) {
+      alert('Введите число!');
+      return;
+   }
+
+   try {
+      // делаем запрос к бесплатному API
+      const response = await fetch(`https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`);
+      const data = await response.json();
+       // const data = {
+       //      result: amount * 90
+       //  };
+      console.log('Data from API:', data);
+
+      if (!data || !data.result) {
+         alert('API не вернул результат. Попробуйте позже.');
+         return;
+      }
+      // показываем результат 
+      const resultEl = document.getElementById('result');
+      resultEl.textContent = `${amount} ${from} = ${data.result.toFixed(2)} ${to}`;
+      resultEl.style.opacity = 1; // плавное появление
+   } catch (error) {
+      alert('Ошибка при получении курса валют. Попробуйте позже.');
+      console.error(error);
+   }
+});
+
